78e2f9520e9e1fd547768573c47ac9b4c4aa3194
Yury <123821125+altLane710@users.noreply.github.com>
2025-03-25 10:52:16 +0300

    Set random bacground instead of fixed color. Add grid size limit.

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 let length = 16
 
+function getRandomColor() {
+  return Math.floor(Math.random() * 256)
+}
+
 function onChangeSize() {
   const ans = prompt('Enter the number of squares per side', length)
 
   if (ans) {
     length = Number(ans)
+    if (length > 100) {
+      alert('Numbers greater than 100 are not allowed')
+      onChangeSize()
+      return
+    }
     destroyGrid();
     createGrid(length)
   }
@@ -25,13 +34,26 @@ function createGrid(length) {
   if (container) {
     for (let i = 0; i < length * length; i++) {
       const div = document.createElement('div')
-      const size = Math.floor(800 / length)
+      const size = Number(800 / length, 2).toFixed(2)
+
       div.classList = 'grid__item'
-      div.style = `height: ${size}px; width: ${size}px`
+      div.style.height = `${size}px`;
+      div.style.width = `${size}px`;
+
       div.addEventListener('mouseover', () => {
-        if (!div.classList.contains('grid__item--visited'))
+        if (div.classList.contains('grid__item--visited')) {
+          if (div.style.opacity > 0) {
+            div.style.opacity -= 0.1;
+          } else {
+            div.style.opacity = 0;
+          }
+        } else {
           div.classList.add('grid__item--visited')
+          div.style.backgroundColor = `rgb(${getRandomColor()},${getRandomColor()},${getRandomColor()})`
+          div.style.opacity = 1;
+        }
       })
+
       container.appendChild(div);
     }
   }
